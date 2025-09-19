7443e5ce7893eb5e8d006e6cf05d770af231931e
Vikasyadav485 <vy15014@gmail.com>
2025-09-19 14:35:10 +0530

    Created a file if_else_practice.js

---
+++ if_else_practice.js
@@ -0,0 +1,9 @@
+const inputStudentAge = 8;
+
+if(inputStudentAge <= 12){
+    console.log("This is a kid.");
+}else if(inputStudentAge <= 19){
+    console.log("This is a teenager.");
+}else{
+    console.log("This is an adult.");
+}
