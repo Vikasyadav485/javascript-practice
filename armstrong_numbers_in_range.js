9d5a03bbc04bc915d804bdde321ff1a62b0f11cc
Vikasyadav485 <vy15014@gmail.com>
2025-09-21 12:34:44 +0530

    Updated for printing all armstrong numbers till 5 digits

--- armstrong_numbers_in_range.js
+++ armstrong_numbers_in_range.js
@@ -1,12 +1,24 @@
-const startOfRange = 1000;
-const endOfRnage = 9999;
+const startOfRange = 1;
+const endOfRnage = 99999;
     
 for(let count = startOfRange; count <= endOfRnage ; count++){
     let inputNum = count;
     let auxilaryNum = inputNum * 10;
     let unitNum = 0;
     let sum = 0;
-    const noOfDigits = 4;
+    let noOfDigits;
+
+    if(count < 10){
+        noOfDigits = 1;
+    }else if(count < 100){
+        noOfDigits = 2;
+    }else if(count < 1000){
+        noOfDigits = 3;
+    }else if(count < 10000){
+        noOfDigits = 4;
+    }else{
+        noOfDigits = 5;
+    }
 
     for(let count1 = 1; count1 <= noOfDigits ;count1++){
         auxilaryNum = (auxilaryNum - unitNum) / 10;
