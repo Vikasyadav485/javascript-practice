9c0197c1c34512502a03bfff1f70a8d15cf83964
Vikasyadav485 <vy15014@gmail.com>
2025-09-21 11:52:52 +0530

    Updated for checking_4_digit_armstrong_number.js

--- check_3_digit_armstrong_number.js
+++ check_3_digit_armstrong_number.js
@@ -1,12 +1,12 @@
-let inputNum = 374;
+let inputNum = 9475;
 let auxilaryNum = inputNum * 10;
 let unitNum = 0;
 let sum = 0;
     
-for(let count1 = 1; count1 <= 3 ;count1++){
+for(let count1 = 1; count1 <= 4 ;count1++){
         auxilaryNum = (auxilaryNum - unitNum) / 10;
         unitNum = auxilaryNum % 10;
-        sum = sum + (unitNum ** 3);
+        sum = sum + (unitNum ** 4);
 }
 
 if(sum === inputNum){
